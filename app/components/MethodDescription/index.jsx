8ec431264fa6d881e6fbf6c7565a21bf9a42f610
Mikhail Eremin <m.eremin@semrush.com>
2017-07-05 17:54:18 +0200

    Add tabs in method description

--- app/components/MethodDescription/index.jsx
+++ app/components/MethodDescription/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
-    
+import { Table, Tabs, Tab, Alert } from 'react-bootstrap';
+import bemCl from 'bem-cl';
+import './MethodDescription.scss';
+
+const b = bemCl('sb-method-description');
+
 class MethodDescription extends React.PureComponent{
     
     resolveType(type, parent) {
@@ -48,31 +52,57 @@ class MethodDescription extends React.PureComponent{
         return rows;
     }
     
+    renderTableIfNeeded() {
+        if (!Object.keys(this.props.schema.properties).length) {
+            return (
+                <Alert bsStyle="warning">
+                    No input properties specified in documentation
+                </Alert>
+            )
+        }
+        
+        return (
+            <Table striped bordered condensed hover>
+                <thead>
+                <tr>
+                    <th>Param</th>
+                    <th>Description</th>
+                    <th>Type</th>
+                    <th>Required</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    Object.keys(this.props.schema.properties).map(
+                        param => this.renderParam(param, this.props.schema.properties)
+                    )
+                }
+                </tbody>
+            </Table>
+        )
+    }
+    
     render() {
         if (!this.props.schema || !this.props.schema.properties) return null;
         
         return (
-            <div>
+            <div className={b()}>
                 <h2>{this.props.method}</h2>
                 <h4>{this.props.schema.description}</h4>
-                <Table striped bordered condensed hover>
-                    <thead>
-                    <tr>
-                        <th>Param</th>
-                        <th>Description</th>
-                        <th>Type</th>
-                        <th>Required</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        Object.keys(this.props.schema.properties).map(
-                            param => this.renderParam(param, this.props.schema.properties)
-                        )
-                    }
-                    </tbody>
-                </Table>
-                
+    
+                <Tabs
+                    defaultActiveKey={1}
+                    id="uncontrolled-tab-example"
+                >
+                    <Tab eventKey={1} title="Input">
+                        {
+                            this.renderTableIfNeeded()
+                        }
+                    </Tab>
+                    <Tab eventKey={2} title="Output">
+                        
+                    </Tab>
+                </Tabs>
                 
             </div>
         )
